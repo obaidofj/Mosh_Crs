af721a74b9bb19f9f7084841e136b1d9c361e395
obaidNearoukh <obaidofj@gmail.com>
2020-06-29 12:28:55 +0300

    Auto-generated commit

--- out-tsc/src/app/common/app-error-handler.js
+++
@@ -1,7 +0,0 @@
-export class AppErrorHandler {
-    handleError(error) {
-        alert('unexpected error ocured');
-        console.log(error);
-    }
-}
-//# sourceMappingURL=app-error-handler.js.map
